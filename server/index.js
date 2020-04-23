6ca668fb8e8c455e2b835310ae1fd3fc48ad6a7a
fracerma <fracerma@users.github.com>
2020-04-24 00:08:19 +0200

    Creata prima idea homepage
    Co-authored-by: Michela Capasso <michela.capasso@icloud.com>
    Co-authored-by: RiccardoCasciotti <riccardo.casciotti.1998@gmail.com>

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
+//Requiring all needed modules
 const express= require("express");
 const bodyParser=require("body-parser");
-
 const app= express();
+
 app.use(bodyParser.json());
 
 app.use("/", express.static(__dirname + "/client/"));
