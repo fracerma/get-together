35a402f84e74381c6d06e041e47944c6811a1c60
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2020-04-29 09:16:56 +0200

    Implementate API cocktail 29/04

--- server/api/cocktails.js
+++ server/api/cocktails.js
@@ -1,8 +1,62 @@
-const express= require("express");
+const express = require("express");
 const router = express.Router();
+const request = require("request");
+const bodyParser = require("body-parser");
+
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json({ type: "application/vnd.api+json" }));
 // define the home page route
-router.get('/', function(req, res) {
-    res.send('Cocktail homepage');
-  });
+router.get("/cocktails", function (req, res) {
+  var city = req.body.city;
+  if (req.body.random == true) {
+    request.get(
+      {
+        url: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
+
+        method: "GET",
+      },
+      function (error, response, body) {
+        console.error("error:", error);
+        console.log("statusCode:", response && response.statusCode);
+        console.log("body:", body);
+        const res = JSON.parse(body);
+        console.log(res);
+      }
+    );
+  } else {
+    var ingredient = req.body.ingredient;
+    request.get(
+      {
+        url:
+          "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" +
+          ingredient,
+        method: "GET",
+      },
+      function (error, response, body) {
+        console.error("error:", error);
+        console.log("statusCode:", response && response.statusCode);
+        //console.log("body:", body);
+        const res = JSON.parse(body);
+        const drink_id = JSON.parse(body).drinks[0].idDrink;
+        request.get(
+          {
+            url:
+              "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" +
+              drink_id,
+            method: "GET",
+          },
+          function (error, response, body) {
+            console.error("error:", error);
+            console.log("statusCode:", response && response.statusCode);
+            console.log("body:", body);
+            const res = JSON.parse(body);
+            //Ho preso il primo cocktail
+          }
+        );
+      }
+    );
+  }
+});
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 //Requiring all needed modules
-const express= require("express");
-const bodyParser=require("body-parser");
-const app= express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/", express.static(__dirname + "/client/"));
 
-const api= require("./api/main");
+const api = require("./api/main");
 
-app.use("/api", api );
+app.use("/api", api);
 
 app.listen(4000);
