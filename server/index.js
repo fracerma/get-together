44118a60e4498c377aae0f6b2ea110b76a0c8b44
fracerma <fracerma@users.github.com>
2020-05-02 17:51:08 +0200

    bho

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const db= require("./models/index");
 
 app.use(bodyParser.json());
 
@@ -10,5 +11,11 @@ app.use("/", express.static(__dirname + "/client/"));
 const api = require("./api/main");
 
 app.use("/api", api);
+
+app.get("/prova",async (req,res)=>{
+    let user2= (await db.User.findOne({where: {id:3}}));
+    //await user1.setFriends(user2);
+    console.log(await user2.getFriends());
+});
 
 app.listen(4000);
