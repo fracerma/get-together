30bd9236d6124bd0adcbf8eb7d1af77da5c8ac91
fracerma <fracerma@users.github.com>
2020-05-02 17:51:08 +0200

    bho

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -1,6 +1,6 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const Recipe = sequelize.define('Ricetta', {
+  const Recipe = sequelize.define('Recipe', {
     title: DataTypes.STRING,
     image: DataTypes.STRING,
     readyInMinutes: DataTypes.INTEGER,
@@ -9,13 +9,15 @@ module.exports = (sequelize, DataTypes) => {
     dishTypes: DataTypes.JSONB ,//arary json
     cuisines: DataTypes.JSONB ,//array json
     diets: DataTypes.JSONB,//array json
-    extendedIngredients: DataTypes.JSONB,//array di json
+    extendedIngredients: DataTypes.JSONB,//array di json,
+    analyzedInstructions: DataTypes.JSONB,
     leng: DataTypes.STRING,
     type: DataTypes.STRING
   }, {});
   Recipe.associate = function(models) {
     // associations can be defined here
-    //TODO associazioni ricetta
+    Recipe.belongsToMany(models.User,{as: "favourites", through: "Favourites",foreignKey:"recipeId"});
+    Recipe.belongsToMany(models.Party, {through: 'PartyRecipe', foreingKey: "recipeId"});
   };
   return Recipe;
 };
