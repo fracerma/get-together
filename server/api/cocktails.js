abda23abd5cdb0bbee67f2614287c0768a3c728a
fracerma <fracerma@users.github.com>
2020-05-03 20:29:09 +0200

    clean file

--- server/api/cocktails.js
+++ server/api/cocktails.js
@@ -6,61 +6,6 @@ const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 router.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// define the home page route
-/*
-router.get("/cocktails", function (req, res) {
-  var city = req.body.city;
-  if (req.body.random == true) {
-    request.get(
-      {
-        url: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
-
-        method: "GET",
-      },
-      function (error, response, body) {
-        console.error("error:", error);
-        console.log("statusCode:", response && response.statusCode);
-        console.log("body:", body);
-        const res = JSON.parse(body);
-        console.log(res);
-      }
-    );
-  } else {
-    var ingredient = req.body.ingredient;
-    request.get(
-      {
-        url:
-          "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" +
-          ingredient,
-        method: "GET",
-      },
-      function (error, response, body) {
-        console.error("error:", error);
-        console.log("statusCode:", response && response.statusCode);
-        //console.log("body:", body);
-        const res = JSON.parse(body);
-        const drink_id = JSON.parse(body).drinks[0].idDrink;
-        request.get(
-          {
-            url:
-              "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" +
-              drink_id,
-            method: "GET",
-          },
-          function (error, response, body) {
-            console.error("error:", error);
-            console.log("statusCode:", response && response.statusCode);
-            console.log("body:", body);
-            const res = JSON.parse(body);
-            //Ho preso il primo cocktail
-          }
-        );
-      }
-    );
-  }
-});
-*/
-
 router.get("/", function (req, res) {
   const query = req.url;
   axios
