1f94ec6d9309f2e05079ea50353a612499c553a0
fracerma <fracerma@users.github.com>
2020-05-03 20:23:42 +0200

    pull changes

---
+++ server/migrations/20200503151215-create-user-party.js
@@ -0,0 +1,38 @@
+"use strict";
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("UserParties", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      partyId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Parties",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("UserParties");
+  },
+};
