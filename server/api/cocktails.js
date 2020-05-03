e55b8c92da7935dacd476e826d18fcdea346c2f6
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2020-05-03 20:10:08 +0200

    CocktailAPI (da finire) + associazioni: UserParty, UserComment

--- server/api/cocktails.js
+++ server/api/cocktails.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const request = require("request");
+const axios = require("axios");
 const bodyParser = require("body-parser");
 
 router.use(bodyParser.json());
@@ -8,6 +8,7 @@ router.use(bodyParser.json());
 router.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // define the home page route
+/*
 router.get("/cocktails", function (req, res) {
   var city = req.body.city;
   if (req.body.random == true) {
@@ -61,5 +62,41 @@ router.get("/cocktails", function (req, res) {
     );
   }
 });
+*/
 
+router.get("/", function (req, res) {
+  const query = req.url;
+  axios
+    .get("https://www.thecocktaildb.com/api/json/v1/1/random.php", {
+      method: "get",
+      responseType: "JSON",
+    })
+    .then(function (res) {
+      let rawData = res.data;
+      let allRaw = rawData.drinks[0];
+      var ingredient = {};
+      let regex = /[0-9]+/;
+      var i = 0;
+      for (property in allRaw) {
+        if (property == "strIngredient" + regex && property.value != null) {
+          ingredient["ingredient" + i] = property.value;
+          i++;
+        }
+      }
+
+      var cleanData = rawData.map(function (data) {
+        let result = {
+          cocktailID: data.drinks[0].idDrink,
+          cocktailName: data.drinks[0].strDrink,
+          cocktailCat: data.drinks[0].strCategory,
+          cocktailType: data.drinks[0].strAlcoholic,
+          instructions: data.drinks[0].strInstructions,
+          photo: data.drinks[0].strDrinkThumb,
+          ingredients: ingredient,
+        };
+      });
+    })
+    .catch(function (error) {})
+    .finally(function (final) {});
+});
 module.exports = router;
