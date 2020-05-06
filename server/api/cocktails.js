862be8f7a8225c47039174d2e33c019a15568084
Riccardo Casciotti <riccardo.casciotti.1998@gmail.com>
2020-05-06 15:27:23 +0200

    Cocktail API --- Finita

--- server/api/cocktails.js
+++ server/api/cocktails.js
@@ -6,61 +6,6 @@ const bodyParser = require("body-parser");
 router.use(bodyParser.json());
 router.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// define the home page route
-/*
-router.get("/cocktails", function (req, res) {
-  var city = req.body.city;
-  if (req.body.random == true) {
-    request.get(
-      {
-        url: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
-
-        method: "GET",
-      },
-      function (error, response, body) {
-        console.error("error:", error);
-        console.log("statusCode:", response && response.statusCode);
-        console.log("body:", body);
-        const res = JSON.parse(body);
-        console.log(res);
-      }
-    );
-  } else {
-    var ingredient = req.body.ingredient;
-    request.get(
-      {
-        url:
-          "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" +
-          ingredient,
-        method: "GET",
-      },
-      function (error, response, body) {
-        console.error("error:", error);
-        console.log("statusCode:", response && response.statusCode);
-        //console.log("body:", body);
-        const res = JSON.parse(body);
-        const drink_id = JSON.parse(body).drinks[0].idDrink;
-        request.get(
-          {
-            url:
-              "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" +
-              drink_id,
-            method: "GET",
-          },
-          function (error, response, body) {
-            console.error("error:", error);
-            console.log("statusCode:", response && response.statusCode);
-            console.log("body:", body);
-            const res = JSON.parse(body);
-            //Ho preso il primo cocktail
-          }
-        );
-      }
-    );
-  }
-});
-*/
-
 function random(top, length){
   let res = new Array(length);
   let i = 0;
@@ -75,15 +20,14 @@ function random(top, length){
   return res;
 }
 
-function cleaner(res, max){
-  let rawData = res.data;
-  var totLength = rawData.drinks.length;
-  var limit;
+function cleaner(rawData, max){
+  let totLength = rawData.drinks.length;
+  let limit;
   if( max > totLength) limit = totLength;
   else limit = max;
 
   let numbers = random(totLength, limit);
-  let cleanData = new Array(limit);
+  let cleanDt = new Array(limit);
   let Ingredient = new Object();
   let Quantity = new Object();
 
@@ -110,7 +54,7 @@ function cleaner(res, max){
         i++;
       }
     }
-    cleanData[cnt] = rawData.drinks.filter(x => x === allRaw).map((data) => {
+    cleanDt[cnt] = rawData.drinks.filter(x => x === allRaw).map((data) => {
         return {
         cocktailID: data.idDrink,
         cocktailName: data.strDrink,
@@ -124,34 +68,191 @@ function cleaner(res, max){
     });
 
   }
+  let tmp = new Array();
+  let i = 0; 
+  for(; i < limit; i++){
+    tmp[i] = (cleanDt[i][0]);
+    // (cleanDt[i][0])
+  }
+  let cleanData = {drinks: tmp}
     return cleanData;
 }
 
 
-router.get("/random", function (req, response) {
-  const query = req.url;
+//works
+router.get("/random" && "/type", function (req, response) {
+  const q = req.query;
+  var num = q.cocktail_num
+  var type = q.cocktail_type;
   axios
-    .get("https://www.thecocktaildb.com/api/json/v1/1/random.php", {
-      method: "get",
-      responseType: "JSON",
-    })
+    .get("https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=" + type)
     .then(function (res) {
-      var cleanData = cleaner(res, 5);
-      response.send(cleanData);
+      let resId = cleaner(res.data, num);
+
+      ////////////////////////////////////
+      let x = resId.drinks.length;
+      
+      var ids = new Array();
+      var array = new Array();
+      var ress = new Array();
+      let i = 0;
+      for (; i < x; i++) 
+        ids.push(resId.drinks[i].cocktailID);
+
+      i = 0;
+
+      for (; i < x; i++) {
+        let url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + ids[i];
+        array.push(axios.get(url));
+      }
+
+      axios.all(array).then(axios.spread((...responses) => {
+        let i;
+        for (i = 0; i < x; i++) {
+          ress.push(responses[i].data.drinks[0]);
+        }
+        let finalRaw = { drinks: ress };
+        let final = cleaner(finalRaw, num)
+        response.send(final);
+      })).catch(function (error) {
+        console.error(error);
+      });
+      //////////////////////////////////////////
     })
-    .catch(function (error) {})
-    .finally(function (final) {});
+    .catch(function (error) {
+      console.error(error);
+    })
+    .finally(function (final) { })
 });
 
+//works
 router.get("/name", function( req, response ){
+  const q = req.query; 
+  var num = q.cocktail_num
 axios
-  .get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+  .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + q.cocktail_name
 )
   .then(function(res){
-    let result = cleaner(res, 2);
+    let result = cleaner(res.data, num);
     response.send(result);
   })
-  .catch()
-  .finally()
+  .catch(function (error) {
+    console.error(error);
+  })
+  .finally(function (final) { })
 });
+
+//works
+router.get("/category", function (req, response) {
+  const q = req.query;
+  var num = q.cocktail_num
+  axios
+    .get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=' + q.cocktail_category
+    )
+    .then(function (res) {
+      let resId = cleaner(res.data, num);
+
+      ////////////////////////////////////
+      let x = resId.drinks.length;
+
+      var ids = new Array();
+      var array = new Array();
+      var ress = new Array();
+      let i = 0;
+      for (; i < x; i++) 
+        ids.push(resId.drinks[i].cocktailID);
+      
+      i = 0;
+
+      for (; i < x; i++) {
+        let url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + ids[i];
+        array.push(axios.get(url));
+      }
+
+      axios.all(array).then(axios.spread((...responses) => {
+        let i;
+        for (i = 0; i < x; i++) {
+          ress.push(responses[i].data.drinks[0]);
+        }
+        let finalRaw = { drinks: ress };
+        let final = cleaner(finalRaw, num)
+        response.send(final);
+      })).catch(function (error) {
+        console.error(error);
+      });
+      //////////////////////////////////////////
+    })
+    .catch(function (error) {
+      console.error(error);
+    })
+    .finally(function (final) { })
+});
+
+//works
+router.get("/ingredient", function (req, response) {
+  const q = req.query;
+  var num = q.cocktail_num
+  axios
+    .get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + q.cocktail_ingredient
+    )
+    .then(function (res) {
+
+      let resId = cleaner(res.data, num); // contiene la risposta del server con i drink in forma di id
+
+      ////////////////////////////////////
+      let x = resId.drinks.length;
+  
+    
+      var ids = new Array();  //ci metto tutti gli id per creare gli url da mettere nelle promise
+      var array = new Array(); //conterra' le promises per axios.all 
+      var ress = new Array(); //array che contiene tutti i drink ma che devono essere passati in cleaner
+
+      let i = 0;
+      for(; i < x; i++)
+        ids.push(resId.drinks[i].cocktailID);
+      
+      i = 0;
+
+      for (; i < x; i++) {
+        let url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + ids[i];
+        array.push(axios.get(url));
+      }
+
+      axios.all(array).then(axios.spread((...responses) => {
+        let i;
+        for (i = 0; i < x; i++) {
+          ress.push(responses[i].data.drinks[0]);
+        }
+        let finalRaw = {drinks: ress}; // contiene i drink nel formato accettato da cleaner
+        let final = cleaner(finalRaw, num) //valore finale
+        response.send(final);
+      })).catch(function (error) {
+        console.error(error);
+      });
+      //////////////////////////////////////////
+    })
+    .catch(function (error) { 
+      console.error(error);
+    })
+    .finally(function (final) { })
+});
+
+
+//prende un id e restituisce tutti i dettagli sul cocktail
+router.get("/full_cocktail", function(req, response){
+  const q = req.query;
+  const id = q.cocktail_id;
+  axios
+    .get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + id)
+    .then(function (res) {
+      let result = cleaner(res.data,  1);
+      response.send(result);
+    })
+    .catch(function (error) { 
+      console.error(error);
+    })
+    .finally(function (final) { })
+});
+
 module.exports = router;
+
