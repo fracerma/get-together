a50c2d734ab1c556b4ade2a1603a1b4d84aa0cad
RiccardoCasciotti <riccardo.casciotti.1998@gmail.com>
2020-05-06 16:47:47 +0200

    UserComment create and destroy

--- server/models/usercomment.js
+++ server/models/usercomment.js
@@ -15,3 +15,20 @@ module.exports = (sequelize, DataTypes) => {
   };
   return UserComment;
 };
+
+
+UserComment.newComment = function(user, comment){
+  return await UserComment.create({ userId: user, CommentId:  comment});
+}
+await UserComment.create({ userId: user, CommentId:  comment}); // combina build e save( che la carica nel db)
+
+UserComment.deleteComment =  function(user, comment){
+  let tuple = this.findAll({
+    where: {
+      userId: user,
+      commentId: comment 
+    }
+  });
+  return await tuple.destroy();
+}
+
