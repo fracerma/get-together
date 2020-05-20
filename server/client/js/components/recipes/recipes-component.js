35ec71a977892c3e6244274cbbc8647fcb51b00c
fracerma <fracerma@users.github.com>
2020-05-20 14:06:12 +0200

    start recipes page

--- server/client/js/components/recipes/recipes-component.js
+++
@@ -1,7 +0,0 @@
-export default{
-    template:`
-    <div>
-        this is your RECIPES
-    </div>
-    `
-}
--- server/client/js/main.js
+++ server/client/js/main.js
@@ -1,7 +1,7 @@
-    import partyCreator from "./components/home_logged/party/partyItems-component.js";
     import navBar from "./components/navBar.js"
     import profile from "./components/profile/profile-component.js"
-    import recipes from "./components/recipes/recipes-component.js"
+    import recipes from "./components/recipes/recipesPage-component.js"
+    import recipeInfo from "./components/recipes/recipeInfo-component.js"
     import parties from "./components/parties/parties-component.js"
     import homeComponent from "./components/home_logged/home-component.js";
 
@@ -11,6 +11,7 @@
             {path:"/_=_",redirect: '/'},
             {path:"/profile",name:"Profile", component: profile},
             {path:"/recipes",name:"Recipes", component: recipes},
+            {path:"/recipes/:id",name:"RecipeInfo", component: recipeInfo},
             {path:"/parties",name:"Parties", component: parties}
         ]
       });
