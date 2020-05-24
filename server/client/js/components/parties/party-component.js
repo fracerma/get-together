7bf382e1f000c08a12b02339579f395937664e11
fracerma <fracerma@users.github.com>
2020-05-24 20:35:29 +0200

    prova

--- server/client/js/components/parties/party-component.js
+++ server/client/js/components/parties/party-component.js
@@ -29,7 +29,7 @@ export default{
         <div v-if="modify" class="title">
             <input class="elmod" v-model="edit.name" type="text"> </input>
         </div>
-        <div class="info">
+        <div class="info-bar">
             <div class="el" v-if="!modify">
                     <svg class="bi about bi-calendar" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd" d="M14 0H2a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2zM1 3.857C1 3.384 1.448 3 2 3h12c.552 0 1 .384 1 .857v10.286c0 .473-.448.857-1 .857H2c-.552 0-1-.384-1-.857V3.857z" clip-rule="evenodd"/>
