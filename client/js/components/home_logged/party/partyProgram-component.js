6f34bc7e9137d3141dcae752a990901c6da2b17c
fracerma <fracerma@users.github.com>
2020-05-27 19:25:23 +0200

    cleaning some file

--- client/js/components/home_logged/party/partyProgram-component.js
+++ client/js/components/home_logged/party/partyProgram-component.js
@@ -2,7 +2,11 @@ import friendsComponent from "./items-components/friends-component.js"
 import settingComponent from "./settingParty-component.js"
 export default{
     props: ["party"],   //proprietà ricevuta dal padre
-    template:`          
+    /*  Uso due component per le impostazioni del party:
+        - settingComponent: per l'inserimento del nome del party la data e l'ora
+        - friendsComponent: per la selezione degli amici da invitare
+    */
+    template:`                 
         <div>
             <h1 class="titlecreateparty">Create your party</h1>
             <div class="partyProgram-component">
@@ -10,7 +14,11 @@ export default{
                 <friendsComponent v-bind:error="friendError" v-bind:partecipants="partecipants"/>
             </div>
         </div>
-    `,
+    `,/* l'evento next step viene ricevuto dal figlio e inviato nuovamente al padre dopo aver fatto
+         un controllo se l'utente abbia selezionato almeno un amico, in caso negativo attraverso la
+         bind su friends component posso notificare gli errori all'utente
+      */
+
     data() {
         return {
             partecipants: this.party.partecipants,
